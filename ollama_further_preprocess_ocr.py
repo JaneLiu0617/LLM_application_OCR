112d1a8683c5293f46f331fb0b73a2d5d5906a1f
Jane Liu <37990003+JaneLiu0617@users.noreply.github.com>
2025-04-07 20:23:03 -0700

    Update ollama_further_preprocess_ocr.py

--- ollama_further_preprocess_ocr.py
+++ ollama_further_preprocess_ocr.py
@@ -61,7 +61,7 @@ def perform_ocr(image_path):
     if base64_image is None:
         return None
 
-    url = "http://localhost:11434/api/chat"
+    url = "http://localhost:****/api/chat" # change into your own one
     headers = {"Content-Type": "application/json"}
     payload = {
         "model": "llama3.2-vision",
